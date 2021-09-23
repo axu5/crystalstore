dac242638181462fe09052c416cbce6ecfa53fe3
Axu <321axu123@gmail.com>
2021-09-23 07:16:41 +0200

    move js to own file

---
+++ client/client.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXh1NSIsImEiOiJja3R3MXRqZTUyZzA5MnBwbnYxYms2azQ2In0.F8QdPD_tBkWuGDBC5qH65Q";
+// @ts-ignore
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [14.481106479257642, 35.92691522821607],
+  zoom: 15,
+});
